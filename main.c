e615dcf9ecc0644175aa883f630cbafd0672a35c
Rafael B G B <fazerafael@gmail.com>
2018-09-02 23:56:03 -0300

    2

--- main.c
+++ main.c
@@ -1,12 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAXI 10000
+typedef struct{
+	char Item[MAXI];
+	int top;
+}pilha;
+ 
+ void i_pilha (pilha *p)
+{
+     p->top = -1;
+}
+int pilha_vazia (pilha *p)
+{
+    if (p->top == -1)
+    {
+         printf ("nada aqui amigao");
+         return 1;
+    }
+    else
+     return 0;
+}
+void push (pilha *p , char x){
+	p->top++;
+	p->Item[p->top] = x ;
+}
+char pop (pilha *p){
+	int aux;
+	if(pilha_vazia(p)== 1){
+		printf ("nada aqui amigao");
+	}
+	else{
+		aux = p->Item[p->top];
+		p->top--;
+		return aux;
+	}
+}
 
 typedef struct{
 	unsigned char r,g,b; 
 }pixel;
 
 int main(){
+	pilha *p = (pilha*)malloc(sizeof(pilha));
+	i_pilha(p);
 
 	FILE *image;
 	FILE *newImage;
@@ -44,17 +81,17 @@ int main(){
 	for(i=0; i<larg;i++){ 
 		for(j=0;j<alt;j++){
 			fscanf(image, "%c", &RGB[i][j].r );
-		   
-			fscanf(image, "%c", &RGB[i][j].g );
-		
-			fscanf(image, "%c", &RGB[i][j].b );
-		
+			push(p , &RGB[i][j].r);
+            fscanf(image, "%c", &RGB[i][j].g );
+            push(p , &RGB[i][j].r);
+	       	fscanf(image, "%c", &RGB[i][j].b );
+	       	push(p , &RGB[i][j].r);
 		}
 	}
 	for(i=0; i<larg;i++){ 
 		for(j=0;j<alt;j++){
 			
-		if((RGB[i][j].r == 255)&&( RGB[i][j].g == 255)&&( RGB[i][j].b == 255)){
+		if((RGB[i][j].r == RGB[1][1].r)&&( RGB[i][j].g == RGB[1][1].g)&&( RGB[i][j].b == RGB[1][1].b)){
 			if(j == larg-1){
 				printf(" \n");
 			}
